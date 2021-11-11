2e60f33cd2d65baf506fef9bcbbd4ad2713cafa4
Koray <koray.goksu76@gmail.com>
2021-11-11 04:26:11 +0300

    v2 Final

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ kettle = excel['kettle']
 davlumbaz = excel['davlumbaz']
 sackuru = excel['sackuru']
 klima = excel['klima']
+esarj = excel['esarj']
+sarjaleti = excel['sarjaleti']
 
 
 ###### zaman ve yuk tanimlama ######
@@ -67,7 +69,8 @@ pv_e = pv_ye/365  ###Günlük üretim
 
 ##Batarya Enerji Depolama
 bat_time = np.ones(24, dtype = np.float16)
-bat_time[0:8]=0
+bat_time[0]=1
+bat_time[1:8]=0
 bat_time[8]=pv_e*(0.035)
 bat_time[9]=pv_e*(0.0864)
 bat_time[10]=pv_e*(0.108)
@@ -83,12 +86,17 @@ bat_time[18:24]=0
 ##Batarya Enerji Depolama
 o=0
 ##Şebekeden Çekilen Enerji Miktarı
-totaltime = np.zeros(24, dtype = np.float16)
+totaltime = np.zeros(24, dtype = np.float32)
+totaltime_1 = np.zeros(24, dtype = np.float32)
+totaltime_2 = np.zeros(24, dtype = np.float32)
+j = np.zeros(24, dtype = np.float32)
 for i in range(24):
-    totaltime[o]= buzdolabi[o]+camasir[o]+bulasik[o]+camasirkur[o]+d_dondurucu[o]+elksup[o]+termosifon[o]+tv[o]+bilgisayar[o]+firin[o]+aydinlatma[o]+mikrodalga[o]+kettle[o]+davlumbaz[o]+sackuru[o]+klima[o]
+    totaltime_1[o]= buzdolabi[o]+d_dondurucu[o]+elksup[o]+termosifon[o]+tv[o]+bilgisayar[o]+firin[o]+aydinlatma[o]+mikrodalga[o]+kettle[o]+davlumbaz[o]+sackuru[o]+klima[o]
+    totaltime_2[o]= esarj[o]+sarjaleti[o]+camasir[o]+bulasik[o]+camasirkur[o]
+    j[o] = esarj[o] + sarjaleti[o] + camasir[o] + bulasik[o] + camasirkur[o]
     o=o+1
 
-
+totaltime= totaltime_1+totaltime_2
 threshold=np.sum(totaltime)/12
 ##Şebekeden Çekilen Enerji Miktarı
 
@@ -98,54 +106,45 @@ p_baslangic=int(pb)
 p_surec=input("Bu tuketim kac saat suruyor?: ")
 p_bitis=int(p_baslangic)+int(p_surec)  ###Tuketimin azaldigi saat (Uyku saati)
 sayac=0
-totaltime_s=totaltime
 battery=1
-enerjisatis=6
+enerjisbt=6
+enerjisatis=0
 bsay=8
 for i in range(10):
     battery = battery+(bat_time[bsay])
     bsay= bsay + 1
     if battery > 6:
-        enerjisatis = battery-enerjisatis
+        enerjisatis = battery-enerjisbt
         battery = 6
+    elif battery > 6 and enerjisatis>0:
+        enerjisatis= enerjisatis+battery-enerjisbt
     elif battery == 6:
         battery = 6
-
-bastuketim=totaltime
-
-#for i in range(int(p_surec))
- #   if
-
-
-if enerjisatis == 6:
-    enerjisatis=0
 
 a = np.ones(24, dtype=int)
 #index olustur
 for i in range(24):
-    if totaltime[i] > threshold and i==18:
+    if totaltime[i] > threshold and i==18 or i==19 or i==20 or i==21 or i==22:
+        sayac = sayac+1
 
-
-
-
-
-for i in range(int(p_surec)+1):
-    index = totaltime[int(p_baslangic)+i:int(p_bitis)]
+#for i in range(int(p_surec)+1):
+ #   index = totaltime[int(p_baslangic)+i:int(p_bitis)]
     # if not np.all(index == 1):   ##indexten çıkıp totaltime_s'imizi düz modüle eşitleyelim ve bir sayac ciktisi alalim
-    if np.sum(index) != int(p_surec) - i:
-        sayac = sayac + 1
-        totaltime_s = a
+  #  if np.sum(index) != int(p_surec) - i:
+   #     sayac = sayac + 1
+    #    totaltime_s = a
 
 ########sayac 24'den büyük olma problemini matlabdeki 24den büyük olma durumunda
 # direk 3 arkaya atması emrini vererek çözdüm # değilse de sayac ve 3 saatlik triple tariff arasında yazdırdm
 x = int(p_baslangic)
 y = int(p_bitis)
-
+totaltime_3 = j
 if sayac != 0:
-    if y + sayac > 24:
-        totaltime_s[y : y + 3] = 3
-    else:
-        totaltime_s[y + sayac : y + sayac + 3] = 3
+    if y + sayac > 19:
+        totaltime_3[y - sayac - int(p_surec)-1: y - sayac] = totaltime_3[x-1 : y]
+        totaltime_3[x:y] = totaltime_2[y - sayac - int(p_surec) -1: y - sayac-1]
+
+shifted=totaltime_3+totaltime_1
 
 plt.title("Enerji Uretim Grafigi")
 plt.plot(zaman1, bat_time, color = 'royalblue', label= 'kwH')
@@ -156,20 +155,31 @@ plt.show()
 
 aylikkar=enerjisatis*0.26*30
 toplamtuketim=np.sum(totaltime)
-shiftedtuketim=np.sum(totaltime_s)
+shiftedtuketim=np.sum(shifted)
 pvuretim=np.sum(bat_time)
 
+notshifted_kwh=totaltime/1000
+shifted_kwh=shifted/1000
+notshifted_kwhtotal=notshifted_kwh-bat_time
+shifted_kwhtotal=shifted_kwh-bat_time
 
-plt.title("LoadShifting Grafigi")
-plt.bar(zaman1, totaltime_s, color = 'tab:red', align = 'center', label= 'Shifted')
-plt.bar(zaman1, totaltime, color = 'tab:blue', align = 'center', label= 'Not Shifted')
+plt.title("LoadShifting Grafigi (GES sistemi olmadan)")
+plt.bar(zaman1, shifted_kwh, color = 'tab:red', align = 'center', label= 'Shifted')
+plt.bar(zaman1, notshifted_kwh, color = 'tab:blue', align = 'center', label= 'Not Shifted')
+plt.ylabel("Yük")
+plt.legend()
+plt.show()
+
+plt.title("LoadShifting Grafigi (GES sistemi ile)")
+plt.bar(zaman1, shifted_kwhtotal, color = 'tab:red', align = 'center', label= 'Shifted')
+plt.bar(zaman1, notshifted_kwhtotal, color = 'tab:blue', align = 'center', label= 'Not Shifted')
 plt.ylabel("Yük")
 plt.legend()
 plt.show()
 
 print("Toplam kaydırılan yük: {}" .format(sayac))
-print("Loadshifting uygulanmadan önce: {}".format(totaltime))
-print("Loadshifting uygulandıktan sonra: {}".format(totaltime_s))
+print("Loadshifting uygulanmadan önce: {}".format(notshifted_kwhtotal))
+print("Loadshifting uygulandıktan sonra: {}".format(shifted_kwhtotal))
 
 
 
